b2df1b5feb2d9eda5a0aa549ba2babac0fb1c1a5
Barma Vishwanth <72876374+VishwanthBarma@users.noreply.github.com>
2021-06-02 19:06:05 +0530

    Update Connect4.py

--- ConnectFour/src/Connect4.py
+++ ConnectFour/src/Connect4.py
@@ -1,12 +1,19 @@
-def display(board):
+def display(board, columns):
     """Displaying The Board in a systematic pattern
     board -> the main board of the game
     """
+    for i in range(columns):
+        print("     ", (i + 1), end=" ")
+    print()
+
+    num = 1
     for row in board:
+        print(str(num), end=" ")
         for element in row:
             print("|", element, end=" ")
         print("|")
         print()
+        num += 1
 
 
 def rows_cols_validation(rows, columns):
@@ -14,17 +21,17 @@ def rows_cols_validation(rows, columns):
     rows -> No.of rows in the board
     columns -> No.of columns in the board
     """
-    if 4 > rows > 10 and 4 > columns > 10:
+    if 4 >= rows >= 10 and 4 >= columns >= 10:
         print("You can't play connectFour using mentioned rows/columns")
         exit()
 
 
 def input_player_decision():
     """Assigning the symbols for the players"""
-    player = int(input("You are the player 1 : chose your symbol :\n 1 -> ['O'] \n 2 ->  ['X']\n Enter 1 (or) 2 : "))
-    if player == 1:
+    option = int(input("You are the player-1 \nchoose your symbol :\n1 -> ['O'] \n2 ->  ['X']\nEnter 1 (or) 2 : "))
+    if option == 1:
         return "O"
-    elif player == 2:
+    elif option == 2:
         return "X"
     else:
         print("You Entered Invalid Choice")
@@ -100,7 +107,7 @@ def check_rows(slot, board, rows):
     valid1 = "  O    O    O    O  "
     valid2 = "  X    X    X    X  "
     string_row = ""
-    for i in range(len(rows)):
+    for i in range(rows):
         element = board[slot[0]][i]   # here slot[0] indicates the row value of the placed slot
         string_row += str(element) + ""
     if string_row.find(valid1) != -1 or string_row.find(valid2) != -1:
@@ -117,7 +124,7 @@ def check_columns(slot, board, columns):
     valid1 = "  O    O    O    O  "
     valid2 = "  X    X    X    X  "
     string_column = ""
-    for i in range(len(columns)):
+    for i in range(columns):
         element = board[i][slot[1]]  # here slot[1] indicates the column value of the placed slot
         string_column += str(element) + ""
     if string_column.find(valid1) != -1 or string_column.find(valid2) != -1:
@@ -130,7 +137,7 @@ def check_game_status(slot, board, rows, columns):
     check_columns(slot, board, columns)  # Checking in columns
     check_right_diagonal(slot, board, rows, columns)  # Checking in 1st diagonal
     check_left_diagonal(slot, board, rows, columns)  # Checking in 2nd diagonal
-    
+
 
 def place_in_order(choice, coin_place, cols, board):
     """To place the coin(Choice) in the proper slot according to the rules
@@ -144,34 +151,54 @@ def place_in_order(choice, coin_place, cols, board):
     for i in range(cols):
         a = (cols - 1) - i
         if board[a][ind] == "  -  ":
-            board[a][ind] = "  " + choice + "  "
+            board[a][ind] = "  " + str(choice) + "  "
             return [int(a), int(ind)]  # Returning the row and column number of the slot
         else:
             continue
 
 
 if __name__ == "__main__":
-    board_rows = int(input("Enter no.of rows of board : "))
-    board_columns = int(input("Enter no.of columns of the board : "))
+    board_rows = int(input("Enter no.of rows of board between (4-10) inclusively : "))
+    board_columns = int(input("Enter no.of columns of the board between (4-10) inclusively : "))
     rows_cols_validation(board_rows, board_columns)  # Checking for Rows and Columns Validation
     game_board = [["  -  " for i in range(board_columns)] for j in range(board_rows)]
+    # player1, player2 = None, None
+    player1 = input_player_decision()  # Assigning symbols for players according to the user input
+    player2 = ""
+    if player1 == "O":
+        player2 = "X"
+    else:
+        player2 = "O"
+    display(game_board, board_columns)  # Displaying the board
+    play_game = True
+    turn = [player1, player2]
+    turn1 = turn[0]
+    turn1_name = "Player-1"
+    while play_game:
+        print("Enter 'q' for quit the game")
+        choice_of_player = int(input("Enter the column number to drop your choice ({}): ".format(turn1_name)))
 
-    # place_in_order("X", 1, board_columns, game_board)
-    # place_in_order("X", 5, board_columns, game_board)
-    # place_in_order("X", 3, board_columns, game_board)
-    # place_in_order("X", 4, board_columns, game_board)
-    # print(game_board)
-    # display(game_board)
-    # check_rows(game_board)
+        # Quiting the code if the player enters 'q' in selecting the columns
+        if choice_of_player == "q" or choice_of_player == "Q":
+            exit()
 
+        # Checking for invalid column selection by the user
+        if choice_of_player < 1 or choice_of_player > board_columns:
+            print("Input is out of range- Invalid Input")
+            continue
+        #  Placing the coin in the board
+        coin_slot = place_in_order(turn1, choice_of_player, board_columns, game_board)
 
-    # player1 = input_player_decision()
-    # player2 = None
-    #
-    # if player1 == "O":
-    #     player2 = "X"
-    # else:
-    #     player2 = "O"
-    #
-    #
-    # c_num = int(input("Enter the column number to drop your choice : "))
+        # Displaying the Game Board
+        display(game_board, board_columns)
+
+        # Checking for game status
+        check_game_status(coin_slot, game_board, board_rows, board_columns)
+
+        # Changing turns of te player in the output screen
+        if turn1 == turn[0]:
+            turn1 = turn[1]
+            turn1_name = "Player-2"
+        else:
+            turn1 = turn[0]
+            turn1_name = "Player-1"
